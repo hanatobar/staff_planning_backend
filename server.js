f7a159f49089aadf3452bfdac42838d388948b11
hanatobar <hanatobar27@gmail.com>
2026-04-29 23:13:01 +0300

    final debug fix

--- server.js
+++ server.js
@@ -176,4 +176,12 @@ app.listen(HTTP_PORT, "0.0.0.0", () => {
   console.log(`HTTP server running on port ${HTTP_PORT}`);
 });
 
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection:", err);
+});
+
 //main();
