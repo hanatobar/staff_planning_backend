edb453cdcfb0c4772245268fc75a46447fbdf74c
hanatobar <hanatobar27@gmail.com>
2026-05-05 15:53:54 +0300

    email problem fixing port in service

--- services/emailService.js
+++ services/emailService.js
@@ -4,9 +4,12 @@ class EmailService {
   constructor() {
 this.transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   family: 4,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
